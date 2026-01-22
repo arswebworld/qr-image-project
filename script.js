7503ca1df80298b233e3c8758c64aca693f2a704
arswebworld <onlyforme0770@gmail.com>
2026-01-22 12:24:16 +0530

    Update loading display and result message

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 setTimeout(() => {
-  document.querySelector(".loader").style.display = "none";
-  document.getElementById("loadingText").style.display = "none";
-  document.getElementById("result").style.display = "block";
+  document.getElementById("loading").style.display = "none";
+
+  
+  document.getElementById("resultText").innerHTML =
+    "😅 Sorry! কাউকে খুঁজে পাইনি…<br>Maybe destiny is still loading 💔";
+
+  document.getElementById("resultText").style.display = "block";
 }, 3000);
